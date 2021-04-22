5661b95a95daa553622bda43bdc0e2bf9bcd8c2c
Pbharathwajan <47109417+Pbharathwajan@users.noreply.github.com>
2021-04-22 18:44:01 +0530

    pywebview

---
+++ junk/joe.js
@@ -0,0 +1,14 @@
+var func = function() {
+    var x = document.getElementsByName("operation");
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+
+    if (x[0].checked) {
+        var sum1 = num1 + num2;
+        document.getElementById("name").innerHTML = sum1;
+
+    } else if (x[1].checked) {
+        var diff = num1 - num2
+        document.getElementById("name").innerHTML = diff;
+    }
+}
