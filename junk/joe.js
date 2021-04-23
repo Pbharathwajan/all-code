880796e8ad2d0c92460263587064973572c6b38b
Parthasarathi <parthasarathibharathwajan@gmail.com>
2021-04-23 13:55:26 +0530

    lhlkjh

--- junk/joe.js
+++ junk/joe.js
@@ -1,14 +1,18 @@
-var func = function() {
-    var x = document.getElementsByName("operation");
+function joe() {
+    var addcheck = document.getElementById("add").checked;
+    var subcheck = document.getElementById("sub").checked;
     var num1 = parseInt(document.getElementById("num1").value);
     var num2 = parseInt(document.getElementById("num2").value);
 
-    if (x[0].checked) {
-        var sum1 = num1 + num2;
-        document.getElementById("name").innerHTML = sum1;
 
-    } else if (x[1].checked) {
-        var diff = num1 - num2
-        document.getElementById("name").innerHTML = diff;
+    if (addcheck) {
+        var sum = num1 + num2
+        document.getElementById("result").innerHTML = sum;
     }
+    if (subcheck) {
+        var diff = num1 - num2
+        document.getElementById("result").innerHTML = diff;
+    }
+
+
 }
